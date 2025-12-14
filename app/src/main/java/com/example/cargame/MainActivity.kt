900182367bc82d9d5a1497c5d334b6e61899ea97
tarkhanov33-ui <Tarkhanov33@gmail.com>
2025-12-14 17:13:04 +0200

    Reset project

--- app/src/main/java/com/example/cargame/MainActivity.kt
+++ app/src/main/java/com/example/cargame/MainActivity.kt
@@ -5,89 +5,83 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import kotlin.random.Random
-import android.os.VibrationEffect;
-import android.os.Vibrator;
+
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var car: ImageView
     private lateinit var btnLeft: ImageButton
     private lateinit var btnRight: ImageButton
-    private lateinit var livesLayout: LinearLayout
+
     private lateinit var life1: ImageView
     private lateinit var life2: ImageView
     private lateinit var life3: ImageView
 
-    private lateinit var RockLeft: ImageView
-    private lateinit var RockMid: ImageView
-    private lateinit var RockRight: ImageView
+    private lateinit var car0: ImageView
+    private lateinit var car1: ImageView
+    private lateinit var car2: ImageView
+    private lateinit var cars: Array<ImageView>
 
-    private var lane = 0
+    private lateinit var viewsArray: Array<Array<ImageView>>
+    private val lMatrix = Array(5) { IntArray(3) { 0 } }
+
+    private var lane = 0        // -1 left, 0 mid, 1 right
     private var lives = 3
-
-    private lateinit var laneCenters: FloatArray
-
-    private lateinit var rockViews: Array<ImageView>
-    private lateinit var rockY: FloatArray
-    private lateinit var rockActive: BooleanArray
+    private var counter = 0
 
     private val rockHandler = Handler(Looper.getMainLooper())
-    private val rockStep = 25f
-    private val rockDelay = 50L
+    private val rockDelay = 700L
+    private var timerOn = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        car = findViewById(R.id.car)
         btnLeft = findViewById(R.id.btnLeft)
         btnRight = findViewById(R.id.btnRight)
-        livesLayout = findViewById(R.id.livesLayout)
+
         life1 = findViewById(R.id.life1)
         life2 = findViewById(R.id.life2)
         life3 = findViewById(R.id.life3)
 
-        RockLeft = findViewById(R.id.RockLeft)
-        RockMid = findViewById(R.id.RockMid)
-        RockRight = findViewById(R.id.RockRight)
+        car0 = findViewById(R.id.car0)
+        car1 = findViewById(R.id.car1)
+        car2 = findViewById(R.id.car2)
+        cars = arrayOf(car0, car1, car2)
 
-        laneCenters = FloatArray(3)
+        viewsArray = arrayOf(
+            arrayOf(findViewById(R.id.rock00), findViewById(R.id.rock01), findViewById(R.id.rock02)),
+            arrayOf(findViewById(R.id.rock10), findViewById(R.id.rock11), findViewById(R.id.rock12)),
+            arrayOf(findViewById(R.id.rock20), findViewById(R.id.rock21), findViewById(R.id.rock22)),
+            arrayOf(findViewById(R.id.rock30), findViewById(R.id.rock31), findViewById(R.id.rock32)),
+            arrayOf(findViewById(R.id.rock40), findViewById(R.id.rock41), findViewById(R.id.rock42))
+        )
 
-        rockViews = arrayOf(RockLeft, RockMid, RockRight)
-        rockY = FloatArray(3) { 0f }
-        rockActive = BooleanArray(3) { false }
-
-        lives()
-
-        car.post {
-            laneCenters[0] = RockLeft.x + RockLeft.width / 2f
-            laneCenters[1] = RockMid.x + RockMid.width / 2f
-            laneCenters[2] = RockRight.x + RockRight.width / 2f
-
-            car.x = newCarPosition(laneCenters[lane + 1], car.width)
-
-            startRocks()
-        }
+        resetGame()
 
         btnLeft.setOnClickListener {
             lane = moveCar(-1, lane)
-            car.x = newCarPosition(laneCenters[lane + 1], car.width)
+            showCar()
+            checkCollision()
         }
 
         btnRight.setOnClickListener {
             lane = moveCar(1, lane)
-            car.x = newCarPosition(laneCenters[lane + 1], car.width)
+            showCar()
+            checkCollision()
         }
+
+        startRocks()
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -96,58 +90,110 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun startRocks() {
-        rockHandler.post(object : Runnable {
-            override fun run() {
-                moveRocks()
-                rockHandler.postDelayed(this, rockDelay)
-            }
-        })
+    override fun onStop() {
+        super.onStop()
+        stopRocks()
     }
 
-    private fun SpawnRock(laneIndex: Int) {
-        if (rockActive[laneIndex]) return
+    private fun startRocks() {
+        if (timerOn) return
+        timerOn = true
+        rockHandler.postDelayed(rockRunnable, rockDelay)
+    }
 
-        if (Random.nextFloat() < 0.02f) {
-            val rock = rockViews[laneIndex]
-            rockActive[laneIndex] = true
-            rockY[laneIndex] = -rock.height.toFloat()
-            rock.y = rockY[laneIndex]
-            rock.visibility = View.VISIBLE
+    private fun stopRocks() {
+        timerOn = false
+        rockHandler.removeCallbacks(rockRunnable)
+    }
+
+    private val rockRunnable = object : Runnable {
+        override fun run() {
+            if (!timerOn) return
+            moveRocks()
+            rockHandler.postDelayed(this, rockDelay)
         }
+    }
+
+    private fun resetGame() {
+        lives = 3
+        counter = 0
+        lane = 0
+
+        for (i in 0..4) {
+            for (j in 0..2) {
+                lMatrix[i][j] = 0
+            }
+        }
+
+        lives()
+        showCar()
+        drawRocks()
     }
 
     private fun moveRocks() {
-        for (i in 0..2) {
-            if (!rockActive[i]) {
-               SpawnRock(i)
-                continue
+        for (i in 4 downTo 1) {
+            for (j in 0..2) {
+                lMatrix[i][j] = lMatrix[i - 1][j]
             }
+        }
 
-            val rock = rockViews[i]
+        for (j in 0..2) {
+            lMatrix[0][j] = 0
+        }
 
-            rockY[i] += rockStep
-            rock.y = rockY[i]
+        if (counter % 2 == 0) {
+            SpawnRock()
+        }
+        counter++
 
-            if (rockY[i] > car.y - rock.height) {
-                checkCollision(i)
-                rock.visibility = View.INVISIBLE
-                rockActive[i] = false
+        drawRocks()
+        checkCollision()
+    }
+
+    private fun SpawnRock() {
+        val r = Random.nextInt(3)
+        lMatrix[0][r] = 1
+    }
+
+    private fun drawRocks() {
+        for (i in 0..4) {
+            for (j in 0..2) {
+                viewsArray[i][j].visibility =
+                    if (lMatrix[i][j] == 1) View.VISIBLE else View.INVISIBLE
             }
         }
     }
 
-    private fun checkCollision(rockIndex: Int) {
-        val rockLane = rockIndex - 1  // 0→-1, 1→0, 2→+1
-        if (rockLane == lane) {
+    private fun checkCollision() {
+        val laneIndex = lane + 1
+        if (lMatrix[4][laneIndex] == 1) {
+            lMatrix[4][laneIndex] = 0
             lives--
 
-            if (lives <= 0) {
-                lives = 3
-                Toast.makeText(this, "Game Over", Toast.LENGTH_SHORT).show()
-            }
             vibrate()
             lives()
+            drawRocks()
+
+            if (lives <= 0) {
+                gameOver()
+            }
+        }
+    }
+
+    private fun gameOver() {
+        stopRocks()
+        Toast.makeText(this, "Game Over", Toast.LENGTH_SHORT).show()
+
+        rockHandler.postDelayed({
+            resetGame()
+            startRocks()
+        }, 500)
+    }
+
+    private fun showCar() {
+        val idx = lane + 1
+        for (i in 0..2) {
+            cars[i].visibility = if (i == idx) View.VISIBLE else View.INVISIBLE
         }
     }
 
@@ -157,7 +203,6 @@ class MainActivity : AppCompatActivity() {
         life3.visibility = if (lives >= 3) View.VISIBLE else View.INVISIBLE
     }
 
-
     private fun moveCar(input: Int, lane: Int): Int {
         var newLane = lane + input
         if (newLane < -1) newLane = -1
@@ -165,17 +210,14 @@ class MainActivity : AppCompatActivity() {
         return newLane
     }
 
-    private fun newCarPosition(laneCenter: Float, carWidth: Int): Float {
-        return laneCenter - carWidth / 2f
-    }
     private fun vibrate() {
         val v = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        if (!v.hasVibrator()) return
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE))
+            v.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE))
         } else {
-            v.vibrate(500)
+            v.vibrate(200)
         }
     }
-
-
 }
